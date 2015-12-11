ae33a8ae442e82f7f6103fc27d2bac5d3e03bbac
Aiden Tepper <aidenjtep@gmail.com>
2015-12-10 17:59:10 -0800

    Updated webapps2 (finished 5ire)

--- webapps2/5_Into-the-Fire/js/feedback.js
+++ webapps2/5_Into-the-Fire/js/feedback.js
@@ -1,11 +1,55 @@
-$(document).ready(function(){
-  console.log("Document loaded");
+
+var firebase = new Firebase("https://into-the-5ire.firebaseio.com/");
+
+function update(data){
+  $("#feedback-list").html("<br>");
+  for(var fbelem in data){
+    var elem = data[fbelem];
+    var feedbackList =
+    "<div id='" + fbelem + "'><h3><i>" + elem.timestamp + ":</i></h3>" +
+    "<h3>" + elem.fullname + " - " + elem.feedback + "</h3>" +
+    "<h3>" + elem.faves + " favorites</h3>" +
+    "<img id='heart' src='img/heart_outlined.png' width='5%'>" + "</div>";
+    $("#feedback-list").append(feedbackList);
+    $("#feedback-list").append("<br>");
+    $("#" + fbelem).data("id", fbelem);
+    $("#" + fbelem).data("faves", elem.faves);
+  }
+  addEventListeners();
+}
+
+function addEventListeners() {
+
+  $(document).on({
+    mouseenter: function() {
+      $(this).attr("src", "img/heart_filled.png");
+      console.log("filled");
+    },
+    mouseleave: function() {
+      $(this).attr("src", "img/heart_outlined.png");
+      console.log("outlined");
+    }
+  }, "#heart");
+
+  $(document).on({
+    click: function(){
+      var key = $(this).parent().data("id");
+      var faves = $(this).parent().data("faves");
+      faves ++;
+      firebase.child(key).update({
+        "faves": faves
+      });
+    }
+  }, "#heart");
 
   $("#submit_button").click(function(){
     $("#addModal").foundation("reveal", "close");
   });
 
-  var firebase = new Firebase("https://into-the-5ire.firebaseio.com/");
+}
+
+
+function updateDOM() {
 
   firebase.on("value", function(snapshot){
     var data = snapshot.val();
@@ -20,60 +64,27 @@ $(document).ready(function(){
     console.log("The read failed: " + errorObject.code);
   });
 
-  function update(data){
-    $("#feedback-list").html("<br>");
-    for(var fbelem in data){
-      var elem = data[fbelem];
-      if(elem.heart == "true") {
-        var feedbackList =
-        "<div><h3><i>" + elem.timestamp + ":</i></h3>" +
-        "<h3>" + elem.fullname + " - " + elem.feedback + "</h3>" +
-        "<img id='filled' src='img/heart_filled.png' width='5%'>" + "</div>";
-        $("#feedback-list").append(feedbackList);
-        $("#feedback-list").append("<br>");
-      } else if (elem.heart == "false") {
-        var feedbackList =
-        "<div><h3><i>" + elem.timestamp + ":</i></h3>" +
-        "<h3>" + elem.fullname + " - " + elem.feedback + "</h3>" +
-        "<img id='outlined' src='img/heart_outlined.png' width='5%'>" + "</div>";
-        $("#feedback-list").append(feedbackList);
-        $("#feedback-list").append("<br>");
-      }
-    }
-  }
-
   $("#add-form").submit(function(event){
     event.preventDefault();
     console.log("Form submitted");
-
     var fullname = $("#name").val();
     var feedback = $("#feedback").val();
     var date = new Date();
     var timestamp = date.toLocaleDateString() + " at " + date.toLocaleTimeString();
-
     var JSONObj = {
       "fullname":fullname,
       "feedback":feedback,
       "timestamp":timestamp,
-      "heart":"false"
+      "faves": 0
     };
-
     firebase.push(JSONObj, function(){
-      console.log("Push to Firebase was successful: " + JSONObj.fullname);
+      console.log("Push to Firebase was successful");
     });
-
   });
 
-  $("#outlined").hover(function(){
-    $("#outlined").attr("src", "img/heart_filled.png");
-  }, function(){
-    $("#outlined").attr("src", "img/heart_outlined.png");
-  });
+}
 
-  $("#filled").hover(function(){
-    $("#filled").attr("src", "img/heart_outlined.png");
-  }, function(){
-    $("#filled").attr("src", "img/heart_filled.png");
-  });
-
+$(document).ready(function(){
+  updateDOM();
+  console.log("Document loaded");
 });
